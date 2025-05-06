728b2a492ce956914378a2138d34d0503baa6898
rxhxt <rohit37.pict@gmail.com>
2025-05-05 17:15:39 -0700

    Added static html pages

--- Team6/MongooseDB/App.js
+++ Team6/MongooseDB/App.js
@@ -1,36 +1,36 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.App = void 0;
-const express = require("express");
-const bodyParser = require("body-parser");
-const JobPostRoutes_1 = require("./JobPostRoutes");
-const JobPostModel_1 = require("./model/JobPostModel");
-class App {
-    constructor() {
+var express = require("express");
+var bodyParser = require("body-parser");
+var JobPostRoutes_1 = require("./JobPostRoutes");
+var JobPostModel_1 = require("./model/JobPostModel");
+var App = /** @class */ (function () {
+    function App() {
         this.express = express();
         this.middleware();
         this.routes();
     }
-    middleware() {
+    App.prototype.middleware = function () {
         this.express.use(bodyParser.json());
         this.express.use(bodyParser.urlencoded({ extended: false }));
-        this.express.use((req, res, next) => {
+        this.express.use(function (req, res, next) {
             res.header("Access-Control-Allow-Origin", "*");
             res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
             next();
         });
-    }
-    routes() {
-        const dbConnectionString = process.env.DB_INFO || '';
+    };
+    App.prototype.routes = function () {
+        var dbConnectionString = process.env.DB_INFO || '';
         console.log("Using database connection:", dbConnectionString);
-        const jobPostModel = new JobPostModel_1.JobPostModel(dbConnectionString);
-        const jobPostRoutes = new JobPostRoutes_1.JobPostRoutes(jobPostModel);
+        var jobPostModel = new JobPostModel_1.JobPostModel(dbConnectionString);
+        var jobPostRoutes = new JobPostRoutes_1.JobPostRoutes(jobPostModel);
         this.express.use('/', jobPostRoutes.getRouter());
         // Static file serving
         this.express.use('/images', express.static(__dirname + '/img'));
         this.express.use('/data', express.static(__dirname + '/json'));
         this.express.use('/', express.static(__dirname + '/pages'));
-    }
-}
+    };
+    return App;
+}());
 exports.App = App;
-//# sourceMappingURL=App.js.map
