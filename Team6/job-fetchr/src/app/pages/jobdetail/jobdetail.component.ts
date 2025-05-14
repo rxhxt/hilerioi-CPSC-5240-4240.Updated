e6a538282248efb2d06ba529b15b0db016e5738f
Benjamin Philipose <philiposebenjamin@gmail.com>
2025-05-14 14:05:58 -0700

    Initial jobdetail component commit

--- Team6/job-fetchr/src/app/pages/jobdetail/jobdetail.component.ts
+++ Team6/job-fetchr/src/app/pages/jobdetail/jobdetail.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { ActivatedRoute, Params } from '@angular/router';
+import { JobproxyService } from '../../service/jobproxy.service';
 @Component({
   selector: 'app-jobdetail',
   standalone: false,
@@ -7,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './jobdetail.component.css'
 })
 export class JobdetailComponent {
+  job: any;
 
+  constructor(private route: ActivatedRoute, private jobService: JobproxyService) {
+    const id = this.route.snapshot.params['id'];
+    this.jobService.getJobById(id).subscribe((res) => {
+      this.job = res;
+    });
+  }
 }
