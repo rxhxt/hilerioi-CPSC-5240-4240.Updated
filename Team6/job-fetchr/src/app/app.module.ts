09d48a57de7cb7bffd035240bc5dd182f8ed04f3
macnoblebk <macnoblebk@gmail.com>
2025-05-17 11:51:57 -0700

    Refactors file and includes necessary imports

--- Team6/job-fetchr/src/app/app.module.ts
+++ Team6/job-fetchr/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,19 +10,19 @@ import { AppComponent } from './app.component';
 import { JobproxyService } from './service/jobproxy.service';
 import { WelcomepageComponent } from './pages/welcomepage/welcomepage.component';
 import { JoblistingsComponent } from './pages/joblistings/joblistings.component';
-import { JobdetailComponent } from './pages/jobdetail/jobdetail.component';
+//import { JobdetailComponent } from './pages/jobdetail/jobdetail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomepageComponent,
     JoblistingsComponent,
-    JobdetailComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule, 
+    FormsModule
   ],
   providers: [JobproxyService],
   bootstrap: [AppComponent]
