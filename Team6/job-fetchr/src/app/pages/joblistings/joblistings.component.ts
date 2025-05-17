074b4fa81cd541c43cc9d649f3b7e329bc5e12a1
macnoblebk <macnoblebk@gmail.com>
2025-05-17 11:54:36 -0700

    Fixes issues and refactors joblisting component

--- Team6/job-fetchr/src/app/pages/joblistings/joblistings.component.ts
+++ Team6/job-fetchr/src/app/pages/joblistings/joblistings.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { JobproxyService, JobPost } from '../../../jobproxy.service';
 
+
 @Component({
   selector: 'app-joblistings',
   standalone: false,
@@ -11,46 +12,53 @@ import { JobproxyService, JobPost } from '../../../jobproxy.service';
 export class JoblistingsComponent {
   logoPath: string = 'Team6/job-fetchr/src/app/pages/welcomepage/img/logo-white.png';
   jobPosts: JobPost[] = [];
-  loadingJobs: boolean = true;
+  loading: boolean = true;
   errorMessage: string = '';
   searchTerm: string = '';
 
-  constructor( private jobproxyService: JobproxyService, private router: Router){}
+  constructor(private jobproxyService: JobproxyService, private router: Router){}
 
   ngOnInit(): void {
     this.loadJobPosts();
   }
 
   loadJobPosts(): void {
-    this.loadingJobs = true;
+    this.loading = true;
     this.jobproxyService.getAllJobPosts().subscribe({
       next: (data) => {
         this.jobPosts = data;
-        this.loadingJobs = false;
-        console.log('Reteived jobs: ', this.jobPosts);
+        this.loading = false;
+        console.log('Retrieved jobs: ', this.jobPosts);
       },
       error: (error) => {
         this.errorMessage = 'Failed to load job listings. Please try again later.';
-        this.loadingJobs = false;
+        this.loading = false;
         console.log('Error fetching job posts:', error);
       }
     });
   }
 
-  viewJobDetails(jobPostId: string): void{
-    this.router.navigate(['/job', jobPostId]);
+  viewJobDetails(jobPostId: string | undefined): void {
+    if (jobPostId){
+      this.router.navigate(['/job', jobPostId]);
+    }
+    
   }
 
   get filteredJobs(): JobPost[] {
-    if(!this.searchTerm?.trim()) {
+    if (!this.searchTerm?.trim()) {
       return this.jobPosts;
     }
 
     const term = this.searchTerm.toLowerCase();
-    return this.jobPosts.filter( job => 
+    return this.jobPosts.filter(job => 
       job.title.toLowerCase().includes(term) ||
       job.company.toLowerCase().includes(term) || 
       job.location.toLowerCase().includes(term)
     );
+  }
+
+  formatDate(date: Date): string {
+    return new Date(date).toLocaleDateString();
   }
 }
