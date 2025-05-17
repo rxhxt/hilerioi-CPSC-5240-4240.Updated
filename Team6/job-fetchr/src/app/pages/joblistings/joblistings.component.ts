037063c81cfaaef00afc791122c1afcc52138bf7
macnoblebk <macnoblebk@gmail.com>
2025-05-17 10:00:58 -0700

    Adds functionality to request all jobs listing

--- Team6/job-fetchr/src/app/pages/joblistings/joblistings.component.ts
+++ Team6/job-fetchr/src/app/pages/joblistings/joblistings.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { JobproxyService, JobPost } from '../../../jobproxy.service';
 
 @Component({
   selector: 'app-joblistings',
@@ -7,5 +9,30 @@ import { Component } from '@angular/core';
   styleUrl: './joblistings.component.css'
 })
 export class JoblistingsComponent {
+  logoPath: string = '';
+  jobPosts: JobPost[] = [];
+  loadingJobs: boolean = true;
+  errorMessage: string = '';
 
+  constructor( private jobproxyService: JobproxyService, private router: Router){}
+
+  ngOnInit(): void {
+    this.loadJobPosts();
+  }
+
+  loadJobPosts(): void {
+    this.loadingJobs = true;
+    this.jobproxyService.getAllJobPosts().subscribe({
+      next: (data) => {
+        this.jobPosts = data;
+        this.loadingJobs = false;
+        console.log('Reteived jobs: ', this.jobPosts);
+      },
+      error: (error) => {
+        this.errorMessage = 'Failed to load job listings. Please try again later.';
+        this.loadingJobs = false;
+        console.log('Error fetching job posts:', error);
+      }
+    });
+  }
 }
