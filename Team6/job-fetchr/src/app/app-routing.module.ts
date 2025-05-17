c7e5a750d750bdb531c5e2cb5c4adb4f8d03c366
macnoblebk <macnoblebk@gmail.com>
2025-05-17 11:49:46 -0700

    Comments out incomplete Job details component

--- Team6/job-fetchr/src/app/app-routing.module.ts
+++ Team6/job-fetchr/src/app/app-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomepageComponent } from './pages/welcomepage/welcomepage.component';
 import { JoblistingsComponent } from './pages/joblistings/joblistings.component';
-import { JobdetailComponent } from './pages/jobdetail/jobdetail.component';
+//import { JobdetailComponent } from './pages/jobdetail/jobdetail.component';
 
 
 const routes: Routes = [
   { path: '', component: WelcomepageComponent },
-  { path: 'jobs', component: JoblistingsComponent },
-  { path: 'job', component: JobdetailComponent },
-  { path: 'job/:id', component: JobdetailComponent },
+  { path: 'jobs', component: JoblistingsComponent }
+  // { path: 'job', component: JobdetailComponent },
+  // { path: 'job/:id', component: JobdetailComponent },
 ];
 
 @NgModule({
