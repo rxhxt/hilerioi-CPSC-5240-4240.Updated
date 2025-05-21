7bf1a4f9b5f2a78a4e2f9ffc459b03730cbc4e6f
macnoblebk <macnoblebk@gmail.com>
2025-05-20 18:48:42 -0700

    Adds functionality to test job detail end point with retrieved id

--- Team6/Mocha/test/jobpostdetail-api.test.js
+++ Team6/Mocha/test/jobpostdetail-api.test.js
@@ -24,8 +24,19 @@ describe('Test Jobpost Detail results', function () {
                 expect(res.body).to.be.an('array');
                 expect(res.body.length).to.be.greaterThan(0);
                 
-                // Save the ID of the first job post for our detail test
                 jobId = res.body[0].jobPostId;
+                done();
+            });
+    });
+
+    before(function (done) {
+        chai.request("http://localhost:8080")
+            .get("/api/v1/job/" + jobId)
+            .end(function (err, res) {
+                requestResult = res.body;
+                response = res;
+                expect(err).to.be.null;
+                expect(res).to.have.status(200);
                 done();
             });
     });
